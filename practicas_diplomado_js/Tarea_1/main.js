965843a93eebe9f12f171628c3a68d1f6762ada0
Manuel Reyes <Manueldjsr@gmail.com>
2021-02-19 22:26:47 -0400

    Add files via upload
    Tareas

---
+++ practicas_diplomado_js/Tarea_1/main.js
@@ -0,0 +1,14 @@
+let colors = ['#CCFFCC', ' #17e5ec', ' #17b2ec', ' #85ec17'];
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+    console.log("It's working.");
+    
+    // select a ramdom color from color
+    // check this
+    var selectRamdomColor = colors[Math.floor(Math.random()) * colors.length];
+    
+    let setNewcolor = document.getElementById('body');
+
+    setNewcolor.style.background = selectRamdomColor;
+});
